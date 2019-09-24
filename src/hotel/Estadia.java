a56515646af0124283b969908d8745c82dcac652
caiohrgm <caioh_m@hotmail.com>
2019-09-24 09:50:19 -0300

    Modifiquei Animal e adicionei Estadia.

---
+++ src/hotel/Estadia.java
@@ -0,0 +1,43 @@
+package src.hotel;
+
+public class Estadia {
+
+	private Animal animal;
+	private int dias;
+	private double valor;
+	
+	
+	public  Estadia(Animal animal,int dias,double valor) {
+		this.animal = animal;
+		this.dias = dias;
+		this.valor = valor;
+	}
+	
+	public int getDias() {
+		return dias;
+	}
+	
+	public double getValor() {
+		return valor;
+	}
+	
+	public String getNome( ) {
+		return this.animal.getNome();
+	}
+	
+	public String getTipo() {
+		return this.animal.getTipo();
+	}
+	
+	public int getIdade() {
+		return this.animal.getIdade();
+	}
+	
+	@Override
+	public String toString() {
+		String output = getNome() + ", " + getTipo() + ", " + this.dias + " dias com o preço de " + this.valor;
+		return output;
+	}
+	
+	
+}
